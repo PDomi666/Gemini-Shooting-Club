54a1075d083d7c3c168a59682afa7e17f2262831
Pókay Dominik <dominik.pokay@gmail.com>
2024-04-01 18:58:48 +0200

    fegyverek_proba.2

--- script.js
+++ script.js
@@ -38,17 +38,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Get the modal
 var modal = document.querySelector('.modal');
+var modalImg = document.getElementById("modal-image");
+var captionText = document.getElementById("image-caption");
+var prevButton = document.getElementById("prev-button");
+var nextButton = document.getElementById("next-button");
+var currentIndex = 0;
+var currentImages = [];
+var currentCaption = "";
 
 // Get the image, caption and insert them inside the modal
 var images = document.querySelectorAll('.image-gallery img');
-var modalImg = document.getElementById("modal-image");
-var captionText = document.getElementById("image-caption");
 
-images.forEach(function(image) {
+images.forEach(function(image, index) {
   image.addEventListener('click', function() {
+    var imageData = JSON.parse(this.dataset.images);
+    currentImages = imageData;
+    currentIndex = 0;
     modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.textContent = this.dataset.caption; // Set the caption text to the value of the "data-caption" attribute
+    modalImg.src = imageData[currentIndex];
+    currentCaption = this.dataset.caption;
+    updateCaption();
+    updateButtons();
   });
 });
 
@@ -58,4 +68,35 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+};
+
+function updateCaption() {
+  if (currentCaption) {
+    captionText.textContent = currentCaption;
+  } else {
+    captionText.textContent = "Image " + (currentIndex + 1) + " of " + currentImages.length;
+  }
+}
+
+function updateButtons() {
+  prevButton.disabled = currentIndex === 0;
+  nextButton.disabled = currentIndex === currentImages.length - 1;
+}
+
+prevButton.onclick = function() {
+  if (currentIndex > 0) {
+    currentIndex--;
+    modalImg.src = currentImages[currentIndex];
+    updateCaption();
+    updateButtons();
+  }
+};
+
+nextButton.onclick = function() {
+  if (currentIndex < currentImages.length - 1) {
+    currentIndex++;
+    modalImg.src = currentImages[currentIndex];
+    updateCaption();
+    updateButtons();
+  }
 };
